573d0d32238ea7a89c771acc59908a4948bdd094
RAVINDU DEGU G <39943931+Mutant18@users.noreply.github.com>
2019-05-23 14:33:10 +0530

    Add files via upload

---
+++ Program 3/pro3table.sql
@@ -0,0 +1,7 @@
+create table Customer(cnum int,name varchar(20),state varchar(20),climit int,repno int);
+
+create table Representative(repno int,rname varchar(20),state varchar(20),comrate int);
+
+
+
+
